3a842827219e840d13e9de09aca864cb6e85ab68
César López Cortés <cesarlopezcortes@hotmail.com>
2020-11-09 09:41:48 -0600

    Update uome_cnn.php

--- Php/uome_cnn.php
+++ Php/uome_cnn.php
@@ -5,17 +5,17 @@ define('DB_NAME','uome');
 define('DB_USER','root');
 define('DB_PASS','sylka1234');
 
-$conexion = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
+$conexion = mysqli_connect(DB_SERVER, DB_USER, DB_PASS);
 
 if(!$conexion)
 {
-	die('Error MYSQL: '.mysql_error());
+	die('Error MYSQL: '.mysqli_error());
 }
 
-$db = mysql_select_db(DB_NAME,$conexion);
+$db = mysqli_select_db(DB_NAME,$conexion);
 
 if(!$db)
 {
-	die('Error de base de datos: '.mysql_error());
+	die('Error de base de datos: '.mysqli_error());
 }
 ?>
