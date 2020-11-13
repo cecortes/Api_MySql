9712d3bf7af2e44c2c29f7b61b949c8670af7446
César López Cortés <cesarlopezcortes@hotmail.com>
2020-11-12 22:29:47 -0600

    Update uome_sesssion.php

--- Php/uome_sesssion.php
+++ Php/uome_sesssion.php
@@ -5,26 +5,44 @@ $username="root";
 $password="sylka1234";
 
 $json=array();
+//$tuser = "god";
+//$tpass = "god1234";
 
-$conexion=mysqli_connect($hostname,$username,$password,$database);
-		
-$consulta="SELECT * FROM usuarios";
-$resultado=mysqli_query($conexion,$consulta);
+if(isset($_GET["u"]) && isset($_GET["p"])){
+    $user=$_GET['u'];
+	$pwd=$_GET['p'];
 
-if($consulta){
+    $conexion=mysqli_connect($hostname,$username,$password,$database);
 		
-    if($reg=mysqli_fetch_array($resultado)){
-        $json['sesion'][]=$reg;
-    }
+    $consulta="SELECT * FROM usuarios WHERE usr_nom = '{$user}' AND usr_pass = '{$pwd}'";
+    $resultado=mysqli_query($conexion,$consulta);
+
+    if($consulta){
+		
+        if($reg=mysqli_fetch_array($resultado)){
+            $json['sesion'][]=$reg;
+        }
+
     mysqli_close($conexion);
     echo json_encode($json);
+    }
+
+    else{
+        $results["id"]='999';
+        $results["nombre"]='NA';
+        $results["mail"]='NA';
+        $results["pass"]='NA';
+        $results["create"]='NA';
+        $json['sesion'][]=$results;
+        echo json_encode($json);
+    }
 }
 else{
-    $results["id"]='999';
-    $results["nombre"]='NA';
-    $results["mail"]='NA';
-    $results["pass"]='NA';
-    $results["create"]='NA';
+    $results["id"]='';
+    $results["nombre"]='';
+    $results["mail"]='';
+    $results["pass"]='';
+    $results["create"]='';
     $json['sesion'][]=$results;
     echo json_encode($json);
 }
